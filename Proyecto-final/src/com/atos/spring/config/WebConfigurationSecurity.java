ea0a8e79fde5522454b0580faabbd931d9b73339
Cguilled <guillermo6795@gmail.com>
2018-09-11 00:17:30 +0200

    rrrrrr

--- Proyecto-final/src/com/atos/spring/config/WebConfigurationSecurity.java
+++ Proyecto-final/src/com/atos/spring/config/WebConfigurationSecurity.java
@@ -1,25 +1,49 @@
 package com.atos.spring.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.atos.spring.service.UserService;
 
 @Configuration
 @EnableWebSecurity
+@ImportResource("classpath:/com/atos/spring/modelo.xml")
 public class WebConfigurationSecurity extends WebSecurityConfigurerAdapter{
 
+	@Autowired
+	private UserService userService;
 	
+	@Bean
+	public BCryptPasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Override
+	public void configure(AuthenticationManagerBuilder auth)
+	{
+		DaoAuthenticationProvider daoAuth = new DaoAuthenticationProvider();
+		//daoAuth.setPasswordEncoder(encoder());
+		daoAuth.setUserDetailsService(userService); //mi servicio que debe implementar UserDetailsService, solo se usa para logueo
+		auth.authenticationProvider(daoAuth);
+	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/xhtml/**").hasAnyRole("ADMIN","USER").anyRequest().authenticated();
-		http.formLogin().loginPage("/inicio.xhtml").permitAll();
+		//http.authorizeRequests().antMatchers("/xhtml/**").hasAnyRole("ADMIN","USER").anyRequest().authenticated();
+		http.formLogin().loginPage("/login.jsp").permitAll();
 		http.formLogin().loginProcessingUrl("/doLogin").permitAll(); //importante, es la url request del servlet de login de spring security (post)
-		http.formLogin().usernameParameter("username").passwordParameter("password");
+		http.formLogin().usernameParameter("das").passwordParameter("password");
+		http.formLogin().successForwardUrl("/loginSuccess"); //post
 		http.logout().logoutUrl("/doLogout").logoutSuccessUrl("/login.jsp").permitAll();
-		http.exceptionHandling().accessDeniedPage("/control/403");
+		//http.exceptionHandling().accessDeniedPage("/control/403");
 	}
 	
 }
--- Proyecto-final/src/com/atos/spring/service/UserService.java
+++ Proyecto-final/src/com/atos/spring/service/UserService.java
@@ -1,23 +1,54 @@
 package com.atos.spring.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.atos.hibernate.dao.UsuariosDAO;
+import com.atos.hibernate.dto.Roles;
 import com.atos.hibernate.dto.Usuarios;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	//Inyecto un objecto de UsuariosDAO
 	@Autowired 
-	private UsuariosDAO usarios_dao;
+	private UsuariosDAO usuarios_dao;
+
+	@Override
+	public UserDetails loadUserByUsername(String das) throws UsernameNotFoundException {
+		Usuarios usuario = usuarios_dao.findByDas(das);
+		
+		List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+		//authorities a partir de roles
+		Roles roles = usuario.getRoles();
+		
+		
+		GrantedAuthority ga = new GrantedAuthority() {
+			
+			@Override
+			public String getAuthority() {
+				return roles.getDescripcionRol();
+			}
+		};
+		authorities.add(ga);
 	
-	public Usuarios login(String u, String p) {
-		return usarios_dao.findByDasAndPass(u, p);
+		
+		 
+
+		UserDetails userDetails = (UserDetails)new org.springframework.security.core.userdetails.User(usuario.getDAS(),
+				usuario.getPassword(), authorities);
+		
+		return userDetails;
+
 	}
+	
+	
 
 
 }
